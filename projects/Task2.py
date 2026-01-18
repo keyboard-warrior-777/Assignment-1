a2bc567bedc25c1685caf60fd2a711ac5dbfd48d
Sarthak Singh <singhsarthak2425@gmail.com>
2026-01-18 18:25:24 +0530

    Tasks 1 and 2

---
+++ projects/Task2.py
@@ -0,0 +1,4 @@
+first_name = str(input("Enter your first name: "))
+last_name = str(input("Enter your last name: "))
+full_name = first_name + " " + last_name
+print("Hello, " + full_name+"! Welcome to the Python program.",sep="")
